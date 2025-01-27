a7e2e4d2918ae219ea6f6c4c29fe4796ad608f06
MichaelBeaudet <michaeltbeaudetalt@gmail.com>
2025-01-27 10:21:01 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,10 @@
+#Author: Michael Beaudet
+#Title: Week 2 Program 1
+#Date: 1/27/25
 def personal_information():
-    print('...')
+    print('Michael Beaudet')
+    print('123 Fake Street, Apple Valley, MN, 55124')
+    print('123 321 9876')
+    print('Software Engineer')
 
-
-# Line which calls the above function.
 personal_information()
