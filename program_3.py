04ba3b187a10d18f4a16228af361f579dbbe2be4
MichaelBeaudet <michaeltbeaudetalt@gmail.com>
2025-01-28 12:10:05 -0600

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,8 +1,25 @@
+#Author: Michael Beaudet
+#Title: Week 2 Program 3
+#Date: 1/28/25
 def calculate_total_purchase():
-    # A customer in a store is purchasing five items.  
-    # Write a program that asks for each item, 
-    # then displays the subtotal of the sale, 
-    # the amount of sales tax, and the total.  
-    # Assume the sales tax is 7 percent.
+    
+    sales_tax_percent = 0.07
+
+    subtotal = 0 
+
+    item1 = float(input("Enter price of item 1: $"))
+    item2 = float(input("Enter price of item 2: $"))
+    item3 = float(input("Enter price of item 3: $"))
+    item4 = float(input("Enter price of item 4: $"))
+    item5 = float(input("Enter price of item 5: $"))
+
+    subtotal = item1 + item2 +item3 +item4 + item5
+
+    sales_tax = subtotal * sales_tax_percent
+    total = subtotal + sales_tax
+
+    total = round(total, 2)
+
+    print("Total:", total)
 
 calculate_total_purchase()
