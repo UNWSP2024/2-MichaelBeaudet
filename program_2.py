bff8b2321b5525bded2309658c3a739e931fdd92
MichaelBeaudet <michaeltbeaudetalt@gmail.com>
2025-01-28 11:34:28 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,15 @@
-
+#Author: Michael Beaudet
+#Title Week 2 Program 2
+#Date 1/28/25
 def average_age():
-    # Get User Input
+    age1 = int(input("Enter the age of your first friend: "))
+    age2 = int(input("Enter the age of your second friend: "))
+    age3 = int(input("Enter the age of your third friend: "))
+    age4 = int(input("Enter the age of your fourth friend: "))
+    age5 = int(input("Enter the age of your fifth friend: "))
 
+    average = (age1 + age2 +age3 +age4 +age5) / 5
 
-    # Sum ages
+    print('The average age is:',average)
 
-    # Average the ages
-
-    # Print the results
-
-# Line which calls the above function.
 average_age()
